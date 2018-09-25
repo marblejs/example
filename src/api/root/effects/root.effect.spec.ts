c8bda62215ef2af90486d8b871ce1529436d1b44
Jozef Flakus <monodies@me.com>
2018-09-25 21:12:34 +0200

    imp: flattened `root` folder into api/index file

--- src/api/root/effects/root.effect.spec.ts
+++
@@ -1,9 +0,0 @@
-import * as request from 'supertest';
-import { app } from '../../../app';
-
-describe('Root effect', () => {
-  test('GET api/v1 responds with 200', async () =>
-    request(app)
-      .get('/api/v1')
-      .expect(200, '"API version: v1"'));
-});
