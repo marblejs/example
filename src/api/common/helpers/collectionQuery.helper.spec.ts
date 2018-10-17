eae2e8aaeeca6ddf80da850efa3911ae89480bb1
JozefFlakus <monodies@me.com>
2018-10-17 10:33:32 +0200

    test: test cases for total attribute

--- src/api/common/helpers/collectionQuery.helper.spec.ts
+++ src/api/common/helpers/collectionQuery.helper.spec.ts
@@ -16,9 +16,10 @@ describe('#applyCollectionQuery', () => {
     const dbQuery = () => MovieDao.model.find();
 
     // when
-    const { collection } = await applyCollectionQuery(query)(dbQuery);
+    const { collection, total } = await applyCollectionQuery(query)(dbQuery);
 
     // then
+    expect(total).toEqual(4);
     expect(collection[0].metascore! <= collection[1].metascore!).toBe(true);
     expect(collection[1].metascore! <= collection[2].metascore!).toBe(true);
     expect(collection[2].metascore! <= collection[3].metascore!).toBe(true);
@@ -30,9 +31,10 @@ describe('#applyCollectionQuery', () => {
     const dbQuery = () => MovieDao.model.find();
 
     // when
-    const { collection } = await applyCollectionQuery(query)(dbQuery);
+    const { collection, total } = await applyCollectionQuery(query)(dbQuery);
 
     // then
+    expect(total).toEqual(4);
     expect(collection[0].metascore! >= collection[1].metascore!).toBe(true);
     expect(collection[1].metascore! >= collection[2].metascore!).toBe(true);
     expect(collection[2].metascore! >= collection[3].metascore!).toBe(true);
@@ -44,10 +46,12 @@ describe('#applyCollectionQuery', () => {
     const dbQuery = () => MovieDao.model.find();
 
     // when
-    const { collection: coll1 } = await applyCollectionQuery({ ...query, page: 1 })(dbQuery);
-    const { collection: coll2 } = await applyCollectionQuery({ ...query, page: 2 })(dbQuery);
+    const { collection: coll1, total: total1 } = await applyCollectionQuery({ ...query, page: 1 })(dbQuery);
+    const { collection: coll2, total: total2 } = await applyCollectionQuery({ ...query, page: 2 })(dbQuery);
 
     // then
+    expect(total1).toEqual(4);
+    expect(total2).toEqual(4);
     expect(coll1.length).toBe(2);
     expect(coll2.length).toBe(2);
     expect(coll1[0].imdbId).not.toEqual(coll2[0].imdbId);
