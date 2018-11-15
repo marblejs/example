ade86499fea0736292ee403c5a0ca1a574d0c6a0
Jozef Flakus <monodies@me.com>
2018-11-15 22:17:36 +0100

    test: OPTIONS preflight

---
+++ src/api/common/effects/preflight.effect.spec.ts
@@ -0,0 +1,9 @@
+import * as request from 'supertest';
+import { app } from '../../../app';
+
+describe('preflightEffect$', () => {
+  test('GET /api/v1 responds with 200', async () =>
+    request(app)
+      .options('/api/v1')
+      .expect(200));
+});
