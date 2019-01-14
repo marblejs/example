6cda75765e3137493d7241592f7e1defdbdacf8a
Jozef Flakus <monodies@me.com>
2019-01-14 19:33:04 +0100

    imp(docs): apiary.sh script

--- scripts/apiary.sh
+++ scripts/apiary.sh
@@ -3,10 +3,20 @@
 echo "-------------"
 echo "API blueprint"
 
-if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
-  echo "Publishing to marblejs"
-  apiary publish --api-name=marblejs --path ./dist/docs.apib
-else
-  echo "Publishing to marblejs-dev"
-  apiary publish --api-name=marblejsdev --path ./dist/docs.apib
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  echo "Skipping, publishing is enabled only for non-PR builds"
+  exit 0
 fi
+
+ENV=""
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+  ENV="marblejs"
+fi
+
+if [[ $ENV == "" ]]; then
+  echo "Skipping, unsupported environment for branch ${TRAVIS_BRANCH}"
+  exit 0
+fi
+
+echo "Publishing to environment $ENV from branch ${TRAVIS_BRANCH}"
+apiary publish --api-name=$ENV --path ./dist/docs.apib
